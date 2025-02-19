2fb6625f8d0535a2306c01408b5d2450d43f911b
LimZzy21 <rainymasster@gmail.com>
2025-02-19 16:27:48 +0200

    feat: add links to rooms (require for vercel)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -121,19 +121,64 @@ export function Header() {
                     to="/rooms/standart-single"
                     className="block px-4 py-2 "
                   >
-                    Делюкс
+                    стандарт однокімнатний
+                  </Link>
+                  
+                  <Link
+                    to="/rooms/standart-two-bed"
+                    className="block px-4 py-2 "
+                  >
+                    Стандарт з двома ліжками
+                  </Link>
+                 
+                  <Link
+                    to="/rooms/standart-big-bed-balcony"
+                    className="block px-4 py-2 "
+                  >
+                    стандарт велике ліжко з балконом
+                  </Link>
+                  <Link
+                    to="/rooms/standart-two-bed-balcony"
+                    className="block px-4 py-2 "
+                  >
+                    стандарт два ліжка з балконом
+                  </Link>
+                 
+                  <Link
+                    to="/rooms/standart-big-bed"
+                    className="block px-4 py-2 "
+                  >
+                    стандарт з великим ліжком
+                  </Link>
+                  <Link
+                    to="/rooms/standart-two-bed"
+                    className="block px-4 py-2 "
+                  >
+                    стандарт з двома ліжками
                   </Link>
                   <Link
                     to="/rooms/semi-lux"
                     className="block px-4 py-2 "
                   >
-                    Люкс
+                    напів
                   </Link>
                   <Link
-                    to="/rooms/standard-two"
+                    to="/rooms/lux-two-rooms"
                     className="block px-4 py-2 "
                   >
-                    Стандарт
+                    люкс двокімнатний
+                  </Link>
+                  <Link
+                    to="/rooms/lux-imperial"
+                    className="block px-4 py-2 "
+                  >
+                    люкс imperial
+                  </Link>
+                  <Link
+                    to="/rooms/lux-elegant"
+                    className="block px-4 py-2 "
+                  >
+                    люкс elegant
                   </Link>
                 </div>
               )}
