104c723f613a740da094efdef4fb4f8b3bfe9449
LimZzy21 <rainymasster@gmail.com>
2025-02-23 16:16:13 +0200

    feat: finish room slider container

--- src/components/ConferesceService/RoomSliderContainer.tsx
+++ src/components/ConferesceService/RoomSliderContainer.tsx
@@ -7,14 +7,14 @@ interface RoomSliderProps {
 
   export const RoomSliderContainer = ({ images }: RoomSliderProps) => {
     return (
-      <div className="relative w-full h-[811px]">
-        <div className="w-full absolute grid lg:grid-cols-[396px_816px_706px] grid-col-[295px] h-full">
-          <div className="border-[#C7C7C7] border-e h-full"></div>
-          <div className="border-[#C7C7C7] border-e h-full"></div>
-          <div className="border-[#C7C7C7] border-e h-full"></div>
+      <div className="relative flex  justify-center w-full h-[471px] lg:h-[811px] ">
+        <div className="lg:w-full w-[295px]  absolute grid lg:grid-cols-[396px_816px_706px] grid-col-[295px] h-full ">
+          <div className="border-[#C7C7C7] border-x border-e h-full"></div>
+          <div className="border-[#C7C7C7] border-e h-full lg:flex hidden"></div>
+          <div className="border-[#C7C7C7] border-e h-full lg:flex hidden"></div>
         </div>
   
-        <div className=" pt-[79px] w-full">
+        <div className=" lg:pt-[79px] pt-[20.5px] w-full">
           <RoomSlider images={images} />
         </div>
       </div>
