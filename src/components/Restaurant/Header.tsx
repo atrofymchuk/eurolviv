5e489a82006ce7a322e91a2b952362e1ca53dc46
LimZzy21 <rainymasster@gmail.com>
2025-02-24 10:42:11 +0200

    feat: responsive design for terrase header

--- src/components/Restaurant/Header.tsx
+++ src/components/Restaurant/Header.tsx
@@ -2,7 +2,7 @@ import headerImg from "../../assets/Terrase/header.jpg";
 
 export const TerraseHeader = () => {
   return (
-    <div className="relative w-full lg:h-[1157px] h-[821px] flex items-center justify-center flex-col">
+    <div className="relative w-full lg:h-[1157px] h-[808px] flex items-center justify-center flex-col">
       <div className="absolute inset-0 bg-gradient-to-b from-[#252526] via-transparent to-[#25252600] z-[-9]"></div>
       <div className="absolute inset-0 bg-[#25252659] z-[-9]"></div>
 
@@ -26,7 +26,7 @@ export const TerraseHeader = () => {
         на гастрономічній висоті
       </h4>
 
-      <div className="flex space-x-[13px] mt-[88px]">
+      <div className="flex lg:flex-row flex-col lg:space-x-[13px] lg:spact-y-0 space-y-[12px] mt-[88px]">
         <button className="font-cofo font-extrabold uppercase text-white bg-[#8C331B] hover:cursor-pointer px-9 py-3 rounded-4xl text-md">
           меню
         </button>
@@ -35,8 +35,8 @@ export const TerraseHeader = () => {
         </button>
       </div>
 
-      <div className="absolute right-0 top-1/3 transform -translate-y-1/2 rotate-[-90deg] origin-bottom-right bg-[#252526] rounded-full lg:py-[13px] lg:px-[25px]">
-        <p className="uppercase text-white font-cofo font-bold tracking-widest text-lg lg:text-[16px] lg:leading-[20px]  ">
+      <div className=" absolute right-0 lg:top-1/3 lg:bottom-0 bottom-1/3   transform -translate-y-1/2 rotate-[-90deg] origin-bottom-right bg-[#252526] rounded-full py-[13px] px-[25px] ">
+        <p className="uppercase text-white font-cofo font-bold tracking-widest text-[12px] lg:text-[16px] lg:leading-[20px]  ">
           вартість банкету
         </p>
       </div>
