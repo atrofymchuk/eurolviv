87a99bc3023d9f2f9594602bc33c9cddab378973
LimZzy21 <rainymasster@gmail.com>
2025-03-10 14:38:20 +0200

    chore: fix issue with default import in SpecialOffers

--- src/pages/SpecialOffers.tsx
+++ src/pages/SpecialOffers.tsx
@@ -1,4 +1,4 @@
-import SpecialOffersModal from '../components/Modals/SpecialOfferModal'
+import {SpecialOffersModal} from '../components/Modals/SpecialOfferModal'
 import { Cards } from '../components/SpecialOffers/Cards'
 import {Header} from '../components/SpecialOffers/Header'
 
