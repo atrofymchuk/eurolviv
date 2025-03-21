8eb2d99a7de958d4f9df78f46fea02b02aff368a
LimZzy21 <rainymasster@gmail.com>
2025-03-21 12:32:44 +0200

    chore: update resolution in slider Mission component

--- src/components/About/Slider.tsx
+++ src/components/About/Slider.tsx
@@ -13,6 +13,7 @@ import {
 
 import { HiArrowLongRight, HiArrowLongLeft } from "react-icons/hi2";
 import { InViewWrapper } from "../utils/InViewWrapper";
+import cn from "classnames";
 const slides = [
   champagneAboutSlider,
   bathroomAboutSlider,
@@ -52,7 +53,6 @@ const ImageSlider = () => {
   const slideRef = useRef<Slider | null>(null);
 
   return (
-    
     <div className="relative w-full flex justify-center ">
       <Slider {...settings} ref={slideRef} className={`w-full h-full  img`}>
         {slides.map((image, index) => {
@@ -65,18 +65,19 @@ const ImageSlider = () => {
               <img
                 src={image}
                 alt={`slide_${index}`}
-                className={`w-full max-w-[500px]  object-cover  transition-all duration-500  px-2
-              ${
-                isSide
-                  ? "xl:w-[530px] xl:h-[353px] w-[231px] h-[154px] z-5"
-                  : ""
-              }
-              ${isEdge ? "xl:w-[514px] xl:h-[316px] w-[231px] h-[154px]" : ""}
-              ${
-                isActive
-                  ? "xl:w-[510px] xl:h-[400px] w-[290px] max-h-full h-5/6 z-10 scale-[105%] lg:scale-[100%]"
-                  : ""
-              }`}
+                className={cn(
+                  `w-full max-w-[500px]  object-cover  transition-all duration-500  px-2
+              
+            `,
+            {
+                    "2xl:w-[510px] 2xl:h-[700px] xl:w-[390px] xl:h-[500px] w-[290px] lg:w-[350px] lg:h-[400px] md:h-[300px] max-h-full h-5/6 z-10 scale-[105%] lg:scale-[100%]":
+                      isActive,
+                    "2xl:w-[530px] 2xl:h-[353px] xl:w-[370px] xl:h-[253px] lg:w-[300px] lg:h-[213px] w-[231px] h-[154px] z-5":
+                      isSide,
+                    "2xl:w-[514px] 2xl:h-[316px] xl:w-[370px] xl:h-[216px] w-[231px] h-[154px] lg:w-[250px] lg:h-[194px]":
+                      isEdge,
+                  }
+                )}
               />
             </InViewWrapper>
           );
