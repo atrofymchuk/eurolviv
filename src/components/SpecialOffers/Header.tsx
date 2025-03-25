a68df121a6621c174e3ff5fadc374dd983421aba
LimZzy21 <rainymasster@gmail.com>
2025-03-25 10:39:30 +0200

    refactor: update Special offer header styles

--- src/components/SpecialOffers/Header.tsx
+++ src/components/SpecialOffers/Header.tsx
@@ -3,33 +3,32 @@ import { useTranslation } from "react-i18next";
 export const Header = () => {
   const { t } = useTranslation()
   return (
-    <div className="flex items-center justify-center bg-[#252526] 2xl:pt-[117px] xl:pt-[102px] lg:pt-[93px] md:pt-[80px] pt-[73px]   px-9">
+    <div className="flex items-center justify-center bg-[#252526] 2xl:pt-[117px] xl:pt-[102px] lg:pt-[93px] md:pt-[80px] pt-[73px]   px-[38px_40px]">
       <div className="grid w-full  lg:grid-cols-[33.31%_33.43%_33.26%] grid-cols-1 text-[#EDE8E5] text-center lg:text-start max-w-[1720px]">
-        <div className=" border-x  lg:border-e-0 border-[#515151] flex flex-col items-center lg:items-start text-center lg:text-left py-8 px-6 lg:pt-[96px] lg:pl-[31px] ">
+        <div className=" border-x  lg:border-e-0 border-[#515151] flex flex-col items-center lg:items-start text-center lg:text-left py-[53px_49px] xl:py-[53px_80px] lg:py-[53px_79px] px-6 lg:pt-[96px] lg:pl-[31px] ">
           <h1
-            className="uppercase font-cofo text-white leading-tight tracking-tight 
-            text-[36px] sm:text-[48px] md:text-[64px] lg:text-[96px] xl:text-[120px] 2xl:text-[198px] z-1"
+            className="uppercase  text-white lg:leading-[91%] leading-[82%] tracking-[-0.1em] 
+            text-[48px] md:text-[64px] lg:text-[120px] xl:text-[150px] 2xl:text-[198px] z-1"
           >
             {t("specialOffers.title")}
-          </h1>{" "}
+          </h1>
           <h1
-            className="uppercase font-cofo text-white leading-tight tracking-tight lg:hidden flex 
-            text-[36px] sm:text-[48px] md:text-[64px] lg:text-[96px] xl:text-[120px] 2xl:text-[198px] "
+            className="uppercase  text-white lg:leading-[91%] leading-[82%] tracking-[-0.1em] lg:hidden flex 
+            text-[48px]  md:text-[64px]  "
           >
             {t("specialOffers.desc2")}
           </h1>
           <p
-            className="relative uppercase font-cofo text-[12px] sm:text-[14px] md:text-[16px] lg:text-[18px] xl:text-[18px] leading-tight sm:leading-normal 
-             mt-4 sm:mt-6 lg:mt-0 "
+            className="relative uppercase  text-[12px] sm:text-[14px] md:text-[16px] lg:text-[16px] xl:text-[18px] leading-tight 2xl:pt-[51px] xl:pt-[40px] lg:pt-[34px] pt-[20px] 2xl:w-[80%] md:w-[65%] lg:w-[100%]"
           >
             {t("specialOffers.desc")}
           </p>
         </div>
 
-        <div className="border-x border-[#515151]  2xl:pt-[267px] 2xl:ps-[25px]  lg:flex hidden flex-col lg:justify-center lg:pt-10 xl:pt-25 ">
+        <div className="border-x border-[#515151]  2xl:pt-[217px] 2xl:ps-[25px]  lg:flex hidden flex-col lg:justify-center lg:pt-22 xl:pt-16 2xl:pb-[101px]  ">
           <h1
-            className="uppercase font-cofo text-white leading-tight tracking-tight lg:flex hidden 
-            text-[36px] sm:text-[48px] md:text-[64px] lg:text-[96px] xl:text-[120px] 2xl:text-[198px] "
+            className="uppercase  text-white leading-[91%] tracking-[-0.1em] lg:flex hidden 
+            xl:text-[150px] 2xl:text-[198px] 2xl:ps-[25px] lg:ps-[25px] lg:text-[120px] "
           >
             {t("specialOffers.desc2")}
           </h1>
