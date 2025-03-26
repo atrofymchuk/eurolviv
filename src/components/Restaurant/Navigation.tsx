3b88d6fdc92314a8592348c916052a00810edd03
LimZzy21 <rainymasster@gmail.com>
2025-03-26 16:10:36 +0200

    chore: fix review slider styles

--- src/components/Restaurant/Navigation.tsx
+++ src/components/Restaurant/Navigation.tsx
@@ -2,29 +2,39 @@ import { forwardRef } from "react";
 import { HiArrowLongLeft, HiArrowLongRight } from "react-icons/hi2";
 import Slider from "react-slick";
 import cn from "classnames";
-type NavigationToProps ={
-  isTerrasePage:boolean
-}
+type NavigationToProps = {
+  isTerrasePage: boolean;
+};
 
-export const Navigation = forwardRef<Slider, NavigationToProps>(({isTerrasePage}, ref) => {
-  const sliderRef = ref as React.RefObject<Slider>;
-  return (
-    <div className="flex flex-col md:items-end lg:justify-between md:justify-end justify-center items-center ">
-      <div className={cn(`flex ${isTerrasePage ? 'xl:max-w-[95.16%] max-w-[90.31%] ': 'md:max-w-[96.6%] max-w-[89.067%]'}  w-full justify-between lg:mt-0 border-b border-x  border-t  border-[#B3B3B3]`)}>
-        <button
-          className="lg:w-[62px] lg:h-[62px] w-[30px] h-[30px]  flex items-center justify-center text-[#8C331B] border border-[#8C331B] hover:bg-[#8C331B] rounded-full z-20 bg-white hover:text-white mt-[26px] ms-[20px] mb-[44px] md:ms-[88px] xl:mt-[17px] xl:mb-[72px]"
-          onClick={() => sliderRef?.current.slickPrev()}
+export const Navigation = forwardRef<Slider, NavigationToProps>(
+  ({ isTerrasePage }, ref) => {
+    const sliderRef = ref as React.RefObject<Slider>;
+    return (
+      <div className="flex flex-col md:items-end lg:justify-between md:justify-end justify-center items-center ">
+        <div
+          className={cn(
+            `flex ${
+              isTerrasePage
+                ? "xl:max-w-[95.16%] max-w-[90.31%] "
+                : "md:max-w-[96.6%] max-w-[89.1%]"
+            }   justify-between lg:mt-0 border-b border-x  border-t  border-[#B3B3B3] w-full`
+          )}
         >
-          <HiArrowLongLeft className="lg:w-[32px] lg:h-[32px] w-[24px] h-[24px]" />
-        </button>
+          <button
+            className="lg:w-[62px] lg:h-[62px] w-[30px] h-[30px]  flex items-center justify-center text-[#8C331B] border border-[#8C331B] hover:bg-[#8C331B] rounded-full z-20 bg-white hover:text-white mt-[26px] ms-[20px] mb-[44px] md:ms-[88px] xl:mt-[17px] xl:mb-[72px]"
+            onClick={() => sliderRef?.current.slickPrev()}
+          >
+            <HiArrowLongLeft className="lg:w-[32px] lg:h-[32px] w-[24px] h-[24px]" />
+          </button>
 
-        <button
-          className="lg:w-[62px] lg:h-[62px] w-[30px] h-[30px] flex items-center justify-center text-[#8C331B] border border-[#8C331B] hover:bg-[#8C331B] rounded-full z-20 hover:text-white mt-[26px] me-[20px] mb-[44px] md:me-[102px] xl:mb-[72px] xl:mt-[17px] xl:me-[137px]"
-          onClick={() => sliderRef?.current.slickNext()}
-        >
-          <HiArrowLongRight className="lg:w-[32px] lg:h-[32px] w-[24px] h-[24px]" />
-        </button>
+          <button
+            className="lg:w-[62px] lg:h-[62px] w-[30px] h-[30px] flex items-center justify-center text-[#8C331B] border border-[#8C331B] hover:bg-[#8C331B] rounded-full z-20 hover:text-white mt-[26px] me-[20px] mb-[44px] md:me-[102px] xl:mb-[72px] xl:mt-[17px] xl:me-[137px]"
+            onClick={() => sliderRef?.current.slickNext()}
+          >
+            <HiArrowLongRight className="lg:w-[32px] lg:h-[32px] w-[24px] h-[24px]" />
+          </button>
+        </div>
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
--- src/components/Restaurant/ReviewSlider.tsx
+++ src/components/Restaurant/ReviewSlider.tsx
@@ -6,56 +6,66 @@ import { useTranslation } from "react-i18next";
 import cn from "classnames";
 
 type ReviewSliderToProps = {
-  isTerrasePage?:boolean
-}
+  isTerrasePage?: boolean;
+};
 
-export const ReviewSlider = forwardRef<Slider, ReviewSliderToProps>(({isTerrasePage}, ref) => {
-  const { restaurantReviews } = usePagesInfoStore();
-  const { t } = useTranslation();
-  
-  const settings = {
-    infinite: true,
-    speed: 500,
-    rows: 1,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    variableWidth: true,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          variableWidth: true,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          variableWidth: false,
-          slidesPerRow: 3,
-        },
-      },
-    ],
-  };
+export const ReviewSlider = forwardRef<Slider, ReviewSliderToProps>(
+  ({ isTerrasePage }, ref) => {
+    const { restaurantReviews } = usePagesInfoStore();
+    const { t } = useTranslation();
 
-  return ( 
-    <div className="flex flex-col md:items-end items-center justify-end w-full overflow-hidden">
-      <div className={cn(`w-full ${isTerrasePage ? " md:max-w-[95.16%] max-w-[95.31%] " :' md:max-w-[96.6%] max-w-[89.067%]'}  2xl:h-[458px] xl:h-[380px] lg:h-[350px] md:h-[254px]  h-fit md:border-x xl:border border-[#B3B3B3]`)}>
-        <div className="slider-container overflow-visible">
-          <Slider {...settings} ref={ref}>
-            {restaurantReviews.map((el) => (
-              <ReviewSliderCard
-                impression={t(el.impression)}
-                text={t(el.text)}
-                key={el.id}
-              />
-            ))}
-          </Slider>
+    const settings = {
+      infinite: true,
+      speed: 500,
+      rows: 1,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      variableWidth: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            variableWidth: true,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            variableWidth: false,
+            slidesPerRow: 3,
+          },
+        },
+      ],
+    };
+
+    return (
+      <div className="flex flex-col md:items-end items-center justify-end w-full overflow-hidden">
+        <div
+          className={cn(
+            `w-full ${
+              isTerrasePage
+                ? " md:max-w-[95.16%] max-w-[95.31%] "
+                : " md:max-w-[96.60%] max-w-[89.1%]"
+            }  2xl:h-[458px] xl:h-[380px] lg:h-[350px] md:h-[254px]  h-fit md:border-x xl:border border-[#C7C7C7]`
+          )}
+        >
+          <div className="slider-container overflow-visible">
+            <Slider {...settings} ref={ref}>
+              {restaurantReviews.map((el) => (
+                <ReviewSliderCard
+                  impression={t(el.impression)}
+                  text={t(el.text)}
+                  key={el.id}
+                />
+              ))}
+            </Slider>
+          </div>
         </div>
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
