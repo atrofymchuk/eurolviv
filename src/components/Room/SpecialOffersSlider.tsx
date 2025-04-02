ed1b5f2972f908034c6714e4efad787a34e23722
LimZzy21 <rainymasster@gmail.com>
2025-04-03 00:05:15 +0300

    fix:try fix issue with view on phone 2

--- src/components/Room/SpecialOffersSlider.tsx
+++ src/components/Room/SpecialOffersSlider.tsx
@@ -26,20 +26,18 @@ export const SpecialOffersSlider = forwardRef<
     slidesToScroll: 1,
     arrows: false,
     infinite: true,
+
     swipeToSlide: true,
     touchThreshold: 10,
     afterChange: (index: number) => {
       setCurrentSlide(index);
     },
-    adaptiveHeight: false,
-    variableWidth: false,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
           centerPadding: "30px",
-          variableWidth: false,
         },
       },
       {
@@ -47,73 +45,72 @@ export const SpecialOffersSlider = forwardRef<
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          variableWidth: false,
+          variableWidth: true,
         },
       },
     ],
   };
 
   return (
-    <div className="relative w-full box-border">
-      <div className="w-full max-w-[100vw] overflow-hidden">
-        <Slider {...settings} ref={ref} className="special-offers-slider w-full overflow-hidden">
-          {specialOffers.map((el, index) => (
-            <div
-              key={index}
-              className={cn(`border lg:p-5 md:p-4 p-3.5 h-[426px] md:h-[540px] lg:h-[668px] border-t border-[#C7C7C7] relative w-full mx-auto ${
-                index !== specialOffers.length - 1 ? "border-r-0" : ""
-              }`
-              ,{
-                "border-s-0 lg:border-s ": currentSlide === index,
-              },
-              {
-                "border-e-0 ":  specialOffers.length -1,
-              }
-            )}
-            >
-              <InViewWrapper className="relative">
-                <img
-                  src={el.src}
-                  alt={t(el.title)}
-                  className={cn(
-                    `w-[240px] h-[232px] md:w-[340px] md:h-[320px] lg:w-[444px] lg:h-[421px] object-cover max-w-full ${el.classes}`
-                  )}
-                />
-                <div
-                  className={cn(`absolute max-w-[80%] md:max-w-full top-0 left-0`, {
-                    "md:pe-0 pe-[20%]": index === 0,
-                  })}
+    <div className="relative">
+      <Slider {...settings} ref={ref} className="special-offers-slider w-screen">
+        {specialOffers.map((el, index) => (
+          <div
+            key={index}
+            className={cn(`border lg:p-5 md:p-4 p-3.5 h-[426px] md:h-[540px] lg:h-[668px] border-t border-[#C7C7C7] relative   ${
+              index !== specialOffers.length - 1 ? "border-r-0" : ""
+            }`
+            ,{
+              "border-s-0 lg:border-s ": currentSlide === index,
+            },
+            {
+              "border-e-0 ":  specialOffers.length -1,
+            }
+          )}
+          >
+            <InViewWrapper className="relative">
+              <img
+                src={el.src}
+                alt={t(el.title)}
+                className={cn(
+                  `w-[240px] h-[232px] md:w-[340px] md:h-[320px] lg:w-[444px] lg:h-[421px] object-cover ${el.classes}`
+                )}
+              />
+              <div
+                className={cn(`absolute max-w-[80%]  `, {
+                  "md:pe-0 pe-[20%]": index === 0,
+                })}
+              >
+                <p
+                  className={cn(`lg:left-[35px] md:left-[28px] lg:top-[35px] md:top-[28px] flex xl:ms-4.5 xl:mt-4.5  ms-3 mt-3 w-fit
+        uppercase lg:w-fit border bg-[#FFFFFF] text-[#A47762] lg:leading-[20px] md:leading-[16px] xl:py-[12px] z-10  
+        md:py-[11px] py-[7px] px-[12px] xl:px-[16px] md:px-[10px] rounded-full font-cofo-medium lg:text-[16px] md:text-[13px] 
+        text-[10px] leading-[12px] break-words md:[text-wrap:normal] [text-wrap:balance]`)}
                 >
-                  <p
-                    className={cn(`lg:left-[35px] md:left-[28px] lg:top-[35px] md:top-[28px] flex xl:ms-4.5 xl:mt-4.5  ms-3 mt-3 w-fit
-          uppercase lg:w-fit border bg-[#FFFFFF] text-[#A47762] lg:leading-[20px] md:leading-[16px] xl:py-[12px] z-10  
-          md:py-[11px] py-[7px] px-[12px] xl:px-[16px] md:px-[10px] rounded-full font-cofo-medium lg:text-[16px] md:text-[13px] 
-          text-[10px] leading-[12px] break-words md:[text-wrap:normal] [text-wrap:balance]`)}
-                  >
-                    {t(el.offer)}
-                  </p>
-                </div>
-              </InViewWrapper>
-
-              <h4 className="uppercase text-[20px] tracking-[-0.05em] leading-[22px] font-cofo-medium pt-[20px] text-[#252526] md:pt-[24px] lg:pt-[28px] md:text-[22px] lg:text-[24px] md:leading-[24px] lg:leading-[26px]">
-                {t(el.title)}
-              </h4>
-              <p className="uppercase text-[10px] w-[219px] pt-[13px] text-[#8F8F8F] md:text-[14px] lg:text-[16px] md:leading-[15px] lg:leading-[17px] leading-[11px] md:w-[300px] lg:w-fit">
-                {t(el.cardDesc)}
-              </p>
-              <div className="text-start w-full  lg:mb-4 md:mb-3 mb-2 mt-auto">
-                <Link
-                  to="/special-offers"
-                  className="mt-auto font-cofo-medium border border-[#8C331B] text-[#8C331B] px-5.75 py-2.5 xl:px-7.25 xl:py-3 rounded-full uppercase text-[12px] md:text-[14px] xl:text-[16px]
-                   hover:cursor-pointer hover:bg-[#8C331B] hover:text-white transition-colors duration-300 font-cofo"
-                >
-                  {t("buttons.details")}
-                </Link>
+                  {t(el.offer)}
+                </p>
               </div>
+            </InViewWrapper>
+
+            <h4 className="uppercase text-[20px] tracking-[-0.05em] leading-[22px] font-cofo-medium pt-[20px] text-[#252526] md:pt-[24px] lg:pt-[28px] md:text-[22px] lg:text-[24px] md:leading-[24px] lg:leading-[26px]">
+              {t(el.title)}
+            </h4>
+            <p className="uppercase text-[10px] w-[219px] pt-[13px] text-[#8F8F8F] md:text-[14px] lg:text-[16px] md:leading-[15px] lg:leading-[17px] leading-[11px] md:w-[300px] lg:w-fit">
+              {t(el.cardDesc)}
+            </p>
+            <div className="text-start w-full  lg:mb-4 md:mb-3 mb-2 mt-auto">
+              <Link
+                to="/special-offers"
+                className="mt-auto font-cofo-medium border border-[#8C331B] text-[#8C331B] px-5.75 py-2.5 xl:px-7.25 xl:py-3 rounded-full uppercase text-[12px] md:text-[14px] xl:text-[16px]
+                 hover:cursor-pointer hover:bg-[#8C331B] hover:text-white transition-colors duration-300 font-cofo"
+              >
+                {t("buttons.details")}
+              </Link>
             </div>
-          ))}
-        </Slider>
-      </div>
+          </div>
+        ))}
+      </Slider>
+      
     </div>
   );
 });
