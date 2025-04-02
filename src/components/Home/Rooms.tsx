360597a2a34c681741ec7a38b0270da79011294e
LimZzy21 <rainymasster@gmail.com>
2025-04-02 21:27:11 +0300

    fix: update roomslider

--- src/components/Home/Rooms.tsx
+++ src/components/Home/Rooms.tsx
@@ -4,23 +4,22 @@ import { useRef } from "react";
 import Slider from "react-slick";
 import { HiArrowLongLeft, HiArrowLongRight } from "react-icons/hi2";
 import { useTranslation } from "react-i18next";
-import { useRoomStore } from "../../store/useRoomsStore";   
+import { useRoomStore } from "../../store/useRoomsStore";
 export const RoomsHome = () => {
   const sliderRef = useRef<Slider | null>(null);
   const { t } = useTranslation();
-  const {  sliderProps  } = useRoomStore();
-
+  const { sliderProps } = useRoomStore();
 
   return (
     <div className="flex flex-col bg-[#A47762] relative items-center md:items-stretch overflow-x-hidden overflow-y-hidden ">
-      <div className="grid z-0 absolute md:grid-cols-[5.2%_19%_34.7%_25.8%] grid-cols-[89.07%] lg:h-[1580px] md:h-[1000px]  h-[968px] md:items-start justify-center md:justify-start w-full  ">
+      <div className="grid -z-1 absolute md:grid-cols-[5.2%_19%_34.7%_25.8%] grid-cols-[89.07%]  lg:h-[1580px] md:h-[1000px] sm:h-[797px] h-[767px] md:items-start justify-center md:justify-start w-full  ">
         <div className="w-full h-full border-[#C29986] hidden md:block"></div>
         <div className="w-full h-full border-e border-x md:border-x-0 border-[#C29986] "></div>
         <div className="w-full h-full border-x border-[#C29986] hidden md:block"></div>
         <div className="w-full h-full border-e border-[#C29986] hidden md:block"></div>
       </div>
 
-      <div className="grid  items-center justify-center md:grid-cols-[24.7%_45.1%_30.2%] w-full lg:h-[350px] h-[300px] md:h-[400px] grid-cols-[89.07%]   ">
+      <div className="grid  items-center justify-center md:grid-cols-[24.7%_45.1%_30.2%] w-full lg:h-[350px] h-[300px] md:h-[350px] grid-cols-[89.07%]   ">
         <div className="flex flex-col w-full h-full md:justify-center  lg:translate-y-[50px] ">
           <div className="z-4">
             <h1
@@ -31,25 +30,24 @@ export const RoomsHome = () => {
               {t("home.rooms.rooms")}
             </h1>
             <div className="md:hidden pt-[30px] flex flex-col items-center justify-center text-center w-full">
-          <h1 className="uppercase   text-[24px] leading-[90%] tracking-[-7%] relative whitespace-nowrap text-[#EDE8E5]">
-            <span className=" ">
-              {t("home.rooms.forWhat.0")}
-            </span>
-            <span className="">
-              {t("home.rooms.forWhat.1")}
-            </span>
-          </h1>
-        </div>
-            <div className="flex md:flex-row lg:ps-[80px]  md:ps-[60px] flex-col text-start items-center md:items-start  2xl:items-start relative 2xl:h-[200px] xl:h-[190px] lg:h-[180px] md:h-[150px] sm:h-[130px] ">
-              <div className="md:absolute flex md:flex-row flex-col pt-[30px] md:pt-[35px]  space-y-4 md:space-y-0 text-center md:text-start md:gap-[20px] xl:gap-x-[75px]">
-                <p className="uppercase  2xl:w-[433px] xl:w-[400px] lg:w-[330px] md:w-[300px] sm:w-[280px] w-[250px] 2xl:text-[16px] xl:text-[15px] lg:text-[14px] md:text-[13px]  
-                text-[14px]  text-[#E9E9E9] ">
+              <h1 className="uppercase   text-[24px] leading-[90%] tracking-[-7%] relative whitespace-nowrap text-[#EDE8E5]">
+                <span className=" ">{t("home.rooms.forWhat.0")}</span>
+                <span className="">{t("home.rooms.forWhat.1")}</span>
+              </h1>
+            </div>
+            <div className="flex md:flex-row lg:ps-[80px]  md:ps-[60px] flex-col text-start items-center  md:items-start  2xl:items-start relative 2xl:h-[200px] xl:h-[190px] lg:h-[180px] md:h-[150px] sm:h-[130px] ">
+              <div className="md:absolute flex md:flex-row items-center flex-col pt-[16px] md:pt-[35px]  space-y-[30px] md:space-y-0 text-center md:text-start md:gap-[20px] xl:gap-x-[75px]">
+                <p
+                  className="uppercase  2xl:w-[433px] xl:w-[400px] lg:w-[330px] md:w-[350px] sm:w-[280px] w-[calc(100%-14%)] 2xl:text-[16px] xl:text-[15px] lg:text-[14px] md:text-[13px]  
+                text-[12px]  text-[#E9E9E9] "
+                >
                   {t("home.rooms.desc")}
                 </p>
                 <div className="flex justify-center items-center  ">
                   <Link
                     to="/rooms"
-                    className="uppercase text-center block text-[#242425] font-semibold hover:bg-black hover:text-[#EDE8E5]   bg-[#EDE8E5] whitespace-nowrap 2xl:px-[19px] 2xl:py-[11px] xl:px-[20px] xl:py-[12px] lg:px-[18px] lg:py-[10px]  py-[12.5px] px-[14.5px] 
+                    className="uppercase text-center block text-[#242425] font-cofo-medium hover:bg-black hover:text-[#EDE8E5]
+                       bg-[#EDE8E5] whitespace-nowrap 2xl:px-[19px] 2xl:py-[11px] xl:px-[20px] xl:py-[12px] lg:px-[18px] lg:py-[10px]  py-[12.5px] px-[14.5px] 
                     rounded-full hover:cursor-pointer 2xl:text-[16px] lg:text-[15px]  text-[12px]"
                   >
                     {t("buttons.viewRooms")}
@@ -65,7 +63,7 @@ export const RoomsHome = () => {
             <span className="absolute  xl:-translate-x-[280px] lg:-translate-x-[200px] md:-translate-x-[150px] ">
               {t("home.rooms.forWhat.0")}
             </span>
-            <br />  
+            <br />
             <span className="absolute xl:-translate-x-[50px]  md:-translate-x-[50px]">
               {t("home.rooms.forWhat.1")}
             </span>
@@ -73,13 +71,14 @@ export const RoomsHome = () => {
         </div>
       </div>
 
-      <div className="relative max-w-screen z-0 mt-30 md:mt-0 xl:mt-27">
-        <div className="z-10 2xl:h-[700px] xl:h-[650px] lg:h-[570px] md:h-[500px] h-[450px]">
-          <RoomsSlider ref={sliderRef}  sliderProps={sliderProps}/>
+      <div className="relative max-w-screen z-0 mb-[34px] md:mb-0 mt-19 md:mt-0 xl:mt-10 2xl:mb-[121px] xl:mb-[30px]">
+        <div className="z-10 2xl:h-[720px] xl:h-[650px] lg:h-[570px] md:h-[500px] sm:h-[370px] h-[327px]">
+          <RoomsSlider ref={sliderRef} sliderProps={sliderProps} />
         </div>
       </div>
+
       <div className="h-[64px] w-full hidden md:flex">
-        <div className="hidden md:flex  w-full justify-between 2xl:-translate-y-30 xl:-translate-y-38 lg:-translate-y-28 md:-translate-y-20 ">
+        <div className="hidden md:flex  w-full justify-between 2xl:-translate-y-48 xl:-translate-y-38 lg:-translate-y-28 md:-translate-y-30 ">
           <button
             className="2xl:w-[62px] 2xl:h-[62px] xl:w-[58px] xl:h-[58px] lg:w-[54px] lg:h-[54px] md:w-[48px] md:h-[48px] flex items-center justify-center hover:text-[#8C331B] border hover:border-[#8C331B] rounded-full z-20 hover:bg-white text-white mt-[26px] 2xl:ms-[73px] md:ms-[60px] lg:ms-[60px] 2xl:mb-[44px] xl:mb-[40px] lg:mb-[36px]"
             onClick={() => sliderRef.current?.slickPrev()}
--- src/components/Home/RoomsSlider.tsx
+++ src/components/Home/RoomsSlider.tsx
@@ -22,7 +22,13 @@ type RoomsSliderProps = {
 
 export const RoomsSlider = forwardRef<Slider, RoomsSliderProps>(
   ({ sliderProps }, ref) => {
-    const [currentSlide, setCurrentSlide] = useState(1);
+    const [currentSlide, setCurrentSlide] = useState(()=>{
+      if(window.innerWidth <= 640){
+        return 0;
+      }
+      return 1;
+    });
+
     const { t } = useTranslation();
 
     const defaultSettings = {
@@ -32,7 +38,7 @@ export const RoomsSlider = forwardRef<Slider, RoomsSliderProps>(
       slidesToShow: 3.2,
       slidesToScroll: 1,
       centerMode: true,
-      initialSlide: 1,
+      initialSlide: currentSlide,
       centerPadding: "0",
       beforeChange: (_: number, newIndex: number) => setCurrentSlide(newIndex),
       responsive: [
@@ -53,23 +59,30 @@ export const RoomsSlider = forwardRef<Slider, RoomsSliderProps>(
         {
           breakpoint: 768, // md
           settings: {
-            slidesToShow: 1,
-            centerPadding: "15%",
+            slidesToShow: 1.2,
+            centerPadding: "12%",
+            variableWidth: true,
+            loop: false
           },
         },
         {
           breakpoint: 640, // sm
           settings: {
             slidesToShow: 1,
-            centerPadding: "10%",
+            centerPadding: "5%",
+            centerMode: false,
+            infinite: false,
+            variableWidth: true,
           },
         },
         {
           breakpoint: 480,
           settings: {
             slidesToShow: 1,
-            centerPadding: "7%",
+            centerPadding: "0",
+            centerMode: false,
             infinite: false,
+            variableWidth: true,
           },
         },
       ],
@@ -78,8 +91,9 @@ export const RoomsSlider = forwardRef<Slider, RoomsSliderProps>(
     const settings = { ...defaultSettings };
 
     return (
-      <div className="overflow-visible 2xl:w-[97%] xl:ms-[70px]">
-        <Slider {...settings} className="overflow-visible" ref={ref}>
+        <div className="overflow-visible 2xl:w-[97%] xl:ms-[70px] ms-[calc(5.93%)] h-full" >
+        <Slider {...settings} className="overflow-visible flex items-center justify-center home-room-slider" ref={ref}>
+          
           {sliderProps.map((el, index) => {
             const isActive = index === currentSlide;
 
@@ -87,23 +101,17 @@ export const RoomsSlider = forwardRef<Slider, RoomsSliderProps>(
               <div
                 key={index}
                 className={cn(
-                  "transition-all duration-500 ease-in-out px-3",
-                  "flex flex-col items-center justify-between",
+                  " px-3",
+                  "flex flex-col items-center justify-center self-center place-content-center  h-full",
                   {
-                    "z-10": isActive,
-                    "": !isActive,
+                    "z-10 2xl:h-[800px] xl:h-[650px] lg:h-[600px] h-fit": isActive,
+                    "2xl:h-[780px] xl:h-[670px] lg:h-[600px] md:h-[400px] h-fit": !isActive,
                   }
                 )}
               >
                 <div
                   className={cn(
-                    "overflow-hidden h-full transition-all duration-500",
-                    {
-                      "2xl:h-[420px] xl:h-[370px] lg:h-[320px] md:h-[290px] sm:h-[250px] h-[220px] ":
-                        isActive,
-                      "2xl:h-[320px] xl:h-[290px] lg:h-[260px] md:h-[230px] sm:h-[210px] h-[190px] ":
-                        !isActive,
-                    }
+                    "overflow-hidden transition-all duration-500   md:w-full   ",
                   )}
                 >
                   <InViewWrapper>
@@ -111,18 +119,24 @@ export const RoomsSlider = forwardRef<Slider, RoomsSliderProps>(
                       src={el.src}
                       alt={`room $`}
                       className={cn(
-                        "object-cover xl:w-full h-full transition-transform duration-700 md:max-h-full max-h-[200px] w-full",
-                        { "scale-110": isActive }
+                        "object-cover  transition-transform duration-700  h-[168px] w-full",
+                        { "": isActive },
+                        {
+                          "2xl:h-[420px] xl:h-[370px] lg:h-[320px] md:h-[290px] ":
+                            isActive,
+                          "2xl:h-[320px] xl:h-[290px] lg:h-[260px] md:h-[230px]  ":
+                            !isActive,
+                        }
                       )}
                     />
                   </InViewWrapper>
                 </div>
 
-                <div className="mt-[13px] lg:mt-[28px] px-2 w-full">
-                  <div className=" flex items-center justify-center">
+                <div className=" lg:mt-[28px]  lg:w-full md:w-full w-[259px] flex flex-col h-[150px]">
+                  <div className="flex items-center justify-center" >
                     <h4
                       className={cn(
-                        "2xl:text-[32px] xl:text-[26px] lg:text-[22px] md:text-[18px] text-[18px] leading-[104%] tracking-[-0.05em] w-4/5 lg:w-2/3 uppercase font-cofo-medium text-[#EDE8E5] text-center  transition-all duration-500 "
+                        "2xl:text-[32px] 2xl:w-2/3 lg:w-4/5 xl:text-[26px] lg:text-[22px] md:text-[18px] text-[18px] leading-[104%] tracking-[-0.05em]  mt-[13px] lg:mt-0 uppercase font-cofo-medium text-[#EDE8E5] text-center transition-all duration-500"
                       )}
                     >
                       {t(el.title)}
@@ -131,7 +145,7 @@ export const RoomsSlider = forwardRef<Slider, RoomsSliderProps>(
 
                   <div
                     className={cn(
-                      "flex flex-col items-center text-white lg:gap-y-2  gap-y-[6px] transition-all duration-500 text-[12px] lg:text-[16px] "
+                      "flex flex-col items-center text-white lg:gap-y-2 gap-y-[6px] transition-all duration-500 text-[12px] lg:text-[16px]"
                     )}
                   >
                     <div className="flex gap-[3px] lg:gap-2 items-center mt-[11px] lg:mt-[18px]">
@@ -140,7 +154,7 @@ export const RoomsSlider = forwardRef<Slider, RoomsSliderProps>(
                         alt=""
                         className="w-[18px] h-[18px]"
                       />
-                      <p className="uppercase text-[14px] lg:text-[14px] leading-[104%]">
+                      <p className="uppercase text-[12px] lg:text-[14px] leading-[104%]">
                         {t("home.rooms.area")}: {el.area} M<sup>2</sup>
                       </p>
                     </div>
@@ -150,17 +164,17 @@ export const RoomsSlider = forwardRef<Slider, RoomsSliderProps>(
                         alt=""
                         className="w-[18px] h-[18px]"
                       />
-                      <p className="uppercase text-[14px] lg:text-[14px] leading-[104%]">
+                      <p className="uppercase text-[12px] lg:text-[14px] leading-[104%]">
                         {t("home.rooms.guests")}: {el.guests}
                       </p>
                     </div>
                   </div>
 
-                  <div className="mt-[34px] flex justify-center transition-all duration-500 ">
+                  <div className="lg:mt-[34px] mt-auto  flex justify-center transition-all duration-500">
                     {isActive && (
                       <Link
                         to={`/rooms/${el.type}`}
-                        className="border border-[#FFFFFF] uppercase text-[12px] xl:text-[16px] font-cofo-medium w-fit px-[20.5px] py-[10px] xl:px-[30.5px] xl:py-[12px] rounded-full text-[#FFFFFF] hover:text-black hover:bg-[#FFFFFF] transition-colors duration-300"
+                        className="border border-[#FFFFFF] uppercase text-[12px] xl:text-[16px] font-cofo-medium w-fit px-[20.5px] py-[10px] xl:px-[28.5px] xl:py-[12px] rounded-full text-[#FFFFFF] hover:text-black hover:bg-[#FFFFFF] transition-colors duration-300"
                       >
                         {t("buttons.details")}
                       </Link>
