4283772222a9625e8e8b2c1a4255b39860deab39
LimZzy21 <rainymasster@gmail.com>
2025-04-04 17:20:09 +0300

    chore: update review slider resposive

--- src/components/About/ReviewCard.tsx
+++ src/components/About/ReviewCard.tsx
@@ -2,7 +2,7 @@ import { Review } from "../../types/types";
 
 export const  ReviewCard =({ review, reviewHeader }: { review: Review, reviewHeader:string }) =>{
     return (
-        <div className="flex flex-col  lg:w-[533px] lg:max-w-[533px] w-[289px] lg:h-[329px] h-[210px]   bg-[#EDE8E5]  ">
+        <div className="flex flex-col  lg:w-[533px] lg:max-w-[533px] w-[289px] lg:h-[329px] h-[210px]    bg-[#EDE8E5] m ">
         <div className="pt-[16px] ps-[17px] xl:ps-[34px] xl:pt-[33px] ">
           <h1 className="uppercase text-[16px] text-[#A47762]  xl:text-[48px] xl:leading-[60px] leading-[25.2px] font-cofo-medium font-cofo xl:tracking-[-0.04em]">
             {review.name}
@@ -12,7 +12,7 @@ export const  ReviewCard =({ review, reviewHeader }: { review: Review, reviewHea
           </p>
         </div>
       
-        <div className="xl:ps-[33px] xl:mt-[34px] max-h-fit mt-[28px] ps-[17px]  ">
+        <div className="xl:ps-[33px] xl:mt-[34px] max-h-fit mt-[28px]  px-[17px]  ">
         <h2 className="uppercase text-[#A47762] xl:text-[20px] text-[14px]   font-cofo-medium ">{reviewHeader}</h2>
           <p className="uppercase text-[#6B6B6B] xl:pt-[13px] xl:max-w-[476px] text-[12px] xl:text-[18px] xl:leading-[22px] font-cofo leading-[15.1px]">
             {review.text}
--- src/components/About/ReviewSlider.tsx
+++ src/components/About/ReviewSlider.tsx
@@ -8,7 +8,6 @@ const ReviewSlider = memo(forwardRef<Slider, ReviewSliderProps>(({ reviews }, re
   const {t } = useTranslation()
   const settings = {
     initialSlide: 0,
-    slidesToShow: 3, 
     slidesToScroll: 1, 
     speed: 500,
     variableWidth: true,
@@ -19,27 +18,25 @@ const ReviewSlider = memo(forwardRef<Slider, ReviewSliderProps>(({ reviews }, re
       {
         breakpoint: 768, 
         settings: {
-          slidesToShow: 1, 
-          slidesToScroll: 1,
+           
           rows: 2,
-          useTransform:false,
-          variableWidth: true,
+          centerMode: true,
         },
-      },
-    ],
-  };
+        },
+      ],
+    };
 
-  const mixedReviews = useMemo(() => 
-    reviews.ua.flatMap((review, index) => [review, reviews.abroad[index]].filter(Boolean)), 
-    [reviews]
-  )
-  
+    const mixedReviews = useMemo(() => 
+      reviews.ua.flatMap((review, index) => [review, reviews.abroad[index]].filter(Boolean)), 
+      [reviews]
+    )
+    
 
   return (
-    <div className="z-20  w-screen top-0 left-0  ">
-      <Slider {...settings} ref={ref} className="review mt-10">
+    <div className="z-20  w-screen top-0 left-0   ">
+      <Slider {...settings} ref={ref} className="w-[calc(100% + 100px)] mt-10 review md:me-0 -me-2">
         {mixedReviews.map((review, index) => (
-          <div key={index} className="mt-2.5 ">
+          <div key={index} className="mt-2.5 lg: me-4 ">
             <ReviewCard review={review} reviewHeader={t("reviews.review")}/>
           </div>
         ))}
