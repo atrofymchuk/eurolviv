50e73ff8a7fe2ea091a037e9a0271402f94562f1
mt3plg <vladtzm@gmail.com>
2026-05-20 18:15:29 +0300

    polish: align conference menu grid lines with seo block
    use same vertical line positions across menu and seo sections.

--- src/components/ConferesceService/ConferenceMenuContent.tsx
+++ src/components/ConferesceService/ConferenceMenuContent.tsx
@@ -11,19 +11,21 @@ export const ConferenceMenuContent = () => {
 
   return (
     <div className="relative flex items-center justify-center w-full" id="menu">
-      <div className="grid absolute lg:grid-cols-[20.5%_42.9%_35%] grid-cols-1 lg:h-[870px] h-full z-[-1] w-full">
-        <div className="w-full border-[#C7C7C7]"></div>
-        <div className="w-full lg:border-x hidden lg:flex border-[#C7C7C7]"></div>
-        <div className="w-full hidden lg:flex border-[#C7C7C7]"></div>
+      <div
+        className="pointer-events-none absolute inset-0 z-0 hidden lg:block"
+        aria-hidden
+      >
+        <div className="absolute inset-y-0 left-[20.5%] w-px -translate-x-1/2 bg-[#C7C7C7]/70" />
+        <div className="absolute inset-y-0 left-[63.4%] w-px -translate-x-1/2 bg-[#C7C7C7]/70" />
       </div>
 
-      <div className="hidden items-center 2xl:space-x-[24px] xl:space-x-[20px] lg:space-x-[16px] lg:flex px-5 pb-[126px] lg:pb-[100px] w-full justify-center">
+      <div className="relative z-10 hidden items-center 2xl:space-x-[24px] xl:space-x-[20px] lg:space-x-[16px] lg:flex px-5 pb-[126px] lg:pb-[100px] w-full justify-center">
         {imageArray.map((el, index) => (
           <ConferenceMenuCard key={index} img={el.img} text={t(el.text)} />
         ))}
       </div>
 
-      <div className="lg:hidden block w-full">
+      <div className="relative z-10 lg:hidden block w-full">
         <ConferenceMenuMobileCard img={imageArray[0].img} />
       </div>
     </div>
